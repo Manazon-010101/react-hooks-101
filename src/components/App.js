c5039c768ae39f86685c0036ede322e6566f729c
Manazon-010101 <74126115 +Manazon-010101@users.noreply.github.com>
2021-01-02 15:20:43 +0900

    save state with localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useEffect,useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import EventForm from './EventForm'
@@ -7,12 +7,28 @@ import OperationLogs from './OperationLogs'
 import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
 
+const APP_KEY = 'appWithRedux'
+
 const App = () => {
-  const initialState = {
+  const appState = localStorage.getItem(APP_KEY)
+  // このinitialStateはJSのオブジェクトである必要がある
+  // なのでこのappStateはJSONの構造を成すstringとしてstringifyで文字列化しているので
+  // それを元に戻す処理が必要になる
+  // 元に戻してあげてJSのオブジェクトに元に戻す処理をやるのは JSON.parseメソッド
+  const initialState = appState ? JSON.parse(appState) : {
     events: [],
     operationLogs: []
   }
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  // ↓stateを監視させて 何か状態がかわったときにコールバックを実行させる
+  useEffect(() => {
+    // stateはローカルストレージに保存できる形式ではないので文字列化させる必要がある
+    // ↓MDNから          // ↓キーを設定する    //↓sttingifyメソッドを使って文字列化する
+    localStorage.setItem(APP_KEY, JSON.stringify(state))
+  }, [state])
+  // こうすることでこのstateの状態遷移が発生する度にsetItemが実行される
+
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       <div className="container-fluid">
